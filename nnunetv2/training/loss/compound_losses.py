12185362f2669ceeae6d5ce6382a04c1f52f7c27
Dominik Zawlocki <zawlockidominik@gmail.com>
2025-03-04 16:00:49 +0100

    add test print

--- nnunetv2/training/loss/compound_losses.py
+++ nnunetv2/training/loss/compound_losses.py
@@ -18,6 +18,8 @@ class DC_and_CE_loss(nn.Module):
         :param weight_dice:
         """
         super(DC_and_CE_loss, self).__init__()
+
+        print('CE and DC')
         if ignore_label is not None:
             ce_kwargs['ignore_index'] = ignore_label
 
